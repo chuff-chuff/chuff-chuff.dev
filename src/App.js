ab0882524879809450aaa222a0fe2de75234737e
chuff-chuff <norizaki85@gmail.com>
2019-12-18 18:55:56 +0000

    introduce react-helmet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>ちゃふちゃふウェブ | 喋ってWebを作る</title>
+        <meta name="description" content="ちゃふちゃふウェブは喋ること、つまりコミュニケーションを大切にしてWebの制作を行っています。あなたの思いに耳を傾け、私の思いも伝える。あなたと、私と、Webを使うユーザーのことを考える開発者。それがちゃふちゃふウェブです。"></meta>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
